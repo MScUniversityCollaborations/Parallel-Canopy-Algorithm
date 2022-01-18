4371416d2bae25107b77d0977cc9b51c5387c493
zartilas <36131492+zartilas@users.noreply.github.com>
2022-01-18 12:04:57 +0200

    mastrer processes

--- main.cpp
+++ main.cpp
@@ -40,7 +40,6 @@ void generateRandomCoordinates(char rank,int numberOfRandCoordinates) {
                 if(xsign) coordinateX /=  divide;
                 if(ysign) coordinateY /=  divide;
            
-            
                 arrX[i] = coordinateX;
                 arrY[i] = coordinateY;		
             
@@ -65,6 +64,31 @@ void mpi() {
     // Get the rank of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    if (world_rank == ZERO) {
+
+        printf("I am processes ZERO boss\n");
+
+        int number0fPoints; 
+        cout << "Set number of points for clustering: \n"; // User Input
+        cin >> number0fPoints;     // Get user input from the keyboard
+        cout << "Your number is:\n" << number0fPoints; // Display the input value  
+        generateRandomCoordinates(0,number0fPoints);
+        
+    }
+
+
+
+
+
+
+    //We will create the master processes 
+    //Well will create scatter() function (για αποστολη μηνυματος στις υπολοιπες διεργασίες)
+    //Χρειαζόμαστε την scatter() για να στειλουμε ενα κομματι πινακα σε αλλες διεργασίες 
+    //Συγκεκριμένα τα περιεχόμενα της ι-οστης θέσης του πίνακα στέλνονται στην ι-οστη διεργασία
+    //Gather() για να πιαμε πισω τα δεδομενα
+    //Reduce() κανουμε πραξεις με αυτην την fun οταν κατα την διαρκεια επιστροφης των δεδομενων
+
+
 
     // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -72,8 +96,8 @@ void mpi() {
     MPI_Get_processor_name(processor_name, &name_len);
 
     // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors with cpp \n",
-           processor_name, world_rank, world_size);
+    //printf("Hello world from processor %s, rank %d out of %d processors with cpp \n",
+      //     processor_name, world_rank, world_size);
 
 
     // Finalize the MPI environment.
@@ -83,12 +107,9 @@ void mpi() {
 
 int main(int argc, char** argv) {
 
-    int number0fPoints; 
-    cout << "Set number 0f points for clustering: "; // User Input
-    cin >> number0fPoints;     // Get user input from the keyboard
-    cout << "Your number is:\n" << number0fPoints; // Display the input value  
 
-    generateRandomCoordinates(0,number0fPoints);
-   
-    
+    mpi();
+
+    return 0;
+      
 }
